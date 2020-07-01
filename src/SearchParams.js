8e0eb07fdc4fc1a9cfbdb3332e074dffaf37bd9e
Bekki Young <moonbladez@hotmail.com>
2020-07-01 13:28:30 +0200

    added animal options to search

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 export const SearchParams = () => {
-	const location = "Seattle, WA";
-
+	const [location, setLocation] = useState("");
+	const [animal, setAnimal] = useState("dog");
+	console.log(location);
 	return (
 		<div className="search-params">
 			<form action="">
 				<label htmlFor="location">
 					Location
-					<input id="location" value={location} placeholder="location" />
+					<input
+						id="location"
+						value={location}
+						placeholder="location"
+						onChange={(event) => {
+							setLocation(event.target.value);
+						}}
+					/>
 				</label>
+				<label htmlFor="animal">
+					animal
+					<select
+						name=""
+						id="animal"
+						value={animal}
+						onChange={(event) => setAnimal(event.target.value)}
+						onBlur={(event) => setAnimal(event.target.value)}
+					>
+						<option>All</option>
+						{ANIMALS.map((animal) => (
+							<option value={animal}>{animal}</option>
+						))}
+					</select>
+				</label>
+
 				<button>Submit</button>
 			</form>
 		</div>
